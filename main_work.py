d57dcf742663517aabf32160c2ee4b78adf10a66
Tristen Turlington <tristenturlinton@gmail.com>
2026-08-31 13:37:23 -0600

    Co-authored-by: Benjamin Anderson <benjamanderson10@users.noreply.github.com> Co-authored-by: addibru <addibru@users.noreply.github.com> Co-authored-by: Jieling2 <Jieling2@users.noreply.github.com>

--- main_work.py
+++ main_work.py
@@ -1,16 +1,28 @@
 """
 
-Tristen Turlington, Addi, Ben Anderson, Mariah
+Tristen Turlington, Ben Anderson, Mariah, Addi Bruening
 8/31/2026
+
+Our main goals from the job title:
+    - LLM Expertise: Experience developing and training large-scale machine learning models, including post-training techniques to enhance domain knowledge, reasoning capabilities, and model alignment.
+    - You focus on the execution of defined projects. You are responsible for writing clean, efficient code to test specific hypotheses regarding reasoning and alignment.
+    - Experience with molecular modalities (e.g., protein sequences, chemical graphs, and structured molecular data).
+    - A passion for applying frontier AI to drug discovery.
+
+STEPS:
+    1.
 """
 
 import numpy as np
 
 
-def lets_go():
-    return np.sum([2, 4])
 
 
+def step_1():
+    pass
+
+
+ # curl -L -o ~/breast-cancer-detection-unet.zip https://www.kaggle.com/api/v1/datasets/download/utkarshsaxenadn/breast-cancer-detection-unet
 
 if __name__ == "__main__":
-    print(lets_go())
+    print("Hello World")
